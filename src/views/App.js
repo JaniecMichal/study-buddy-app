70576c008815b32e291c33580e673bb2ea2d8f92
Michal Janiec <michal.janiec95@gmail.com>
2021-03-31 00:07:37 +0200

    Changed and adapting to project. Added UserList component to rendering. Created styled-component as MainWrapper to centering elements in the site.

--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,20 @@
-import logo from 'assets/logo.svg';
-import './App.css';
+import React from 'react';
+import { GlobalStyle } from 'assets/GlobalStyles/GlobalStyles.js';
+import { theme } from 'assets/theme/theme.js';
+import { ThemeProvider } from 'styled-components';
+import UserList from 'components/organisms/UserList';
+import { MainWrapper } from './styled';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <MainWrapper>
+          <UserList />
+        </MainWrapper>
+      </>
+    </ThemeProvider>
   );
 }
 
