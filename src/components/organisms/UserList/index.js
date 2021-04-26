b03c8f1f6f04d44ead749b69dabdbafe7a77d68a
Michal Janiec <michal.janiec95@gmail.com>
2021-04-26 23:21:57 +0200

    Added users to local state, added functon which mockAPI, added effect to fetching mockAPI. Added function to delete users from list

--- src/components/organisms/UserList/index.js
+++ src/components/organisms/UserList/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledList, Wrapper } from './styled';
 import UserListItem from 'components/molecules/UserListItem';
 import { usersData } from 'data/usersData.js';
+import { Title } from 'components/atoms/Title';
+
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
 
 const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoadingState] = useState([]);
+
+  useEffect(() => {
+    setLoadingState(true);
+    mockAPI()
+      .then((data) => {
+        setLoadingState(false);
+        setUsers(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
   return (
     <Wrapper>
+      <Title>{isLoading ? 'Loading...' : 'Users List:'}</Title>
       <StyledList>
-        {usersData.map((userData) => (
-          <UserListItem userData={userData} key={userData.name} />
+        {users.map((user) => (
+          <UserListItem
+            userData={user}
+            key={user.name}
+            deleteUser={deleteUser}
+          />
         ))}
       </StyledList>
     </Wrapper>
