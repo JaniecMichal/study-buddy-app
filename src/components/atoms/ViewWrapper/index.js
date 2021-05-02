f03fd49df78625f0eca942f9f23c60951930b0b9
Michal Janiec <michal.janiec95@gmail.com>
2021-05-02 23:39:04 +0200

    Added margin

--- src/components/atoms/ViewWrapper/index.js
+++ src/components/atoms/ViewWrapper/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ViewWrapper = styled.div`
   width: 100%;
   max-width: 500px;
+  margin: 25px;
   box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
   border-radius: 10px;
   padding: 20px 30px;
