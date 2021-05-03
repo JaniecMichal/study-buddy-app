ee05566aaf28d595c623bd4d65c34b84d65d8ebb
Michal Janiec <michal.janiec95@gmail.com>
2021-05-03 15:37:51 +0200

    Created Dashboard view component

---
+++ src/views/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { UserShape } from 'assets/helpers/types';
+import { ViewWrapper } from 'components/atoms/ViewWrapper';
+import UsersList from 'components/organisms/UsersList';
+
+const Dashboard = ({ users, deleteUser, isLoading }) => {
+  return (
+    <ViewWrapper>
+      <UsersList users={users} deleteUser={deleteUser} isLoading={isLoading} />
+    </ViewWrapper>
+  );
+};
+
+Dashboard.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape(UserShape)),
+  isLoading: PropTypes.bool,
+  deleteUser: PropTypes.func,
+};
+
+export default Dashboard;
