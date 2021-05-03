0b434789f533de1ebe4e00e6864f17412116ba51
Michal Janiec <michal.janiec95@gmail.com>
2021-05-03 15:37:19 +0200

    Deleted ViewWrapper

--- src/components/organisms/UsersList/index.js
+++ src/components/organisms/UsersList/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { StyledList } from './styled';
 import UserListItem from 'components/molecules/UserListItem';
 import { Title } from 'components/atoms/Title';
-import { ViewWrapper } from 'components/atoms/ViewWrapper';
 
-const UserList = ({ users, isLoading, deleteUser }) => {
+const UsersList = ({ users, isLoading, deleteUser }) => {
   return (
-    <ViewWrapper>
+    <>
       <Title>{isLoading ? 'Loading...' : 'Users List:'}</Title>
       <StyledList>
         {isLoading ||
@@ -18,8 +17,8 @@ const UserList = ({ users, isLoading, deleteUser }) => {
             />
           ))}
       </StyledList>
-    </ViewWrapper>
+    </>
   );
 };
 
-export default UserList;
+export default UsersList;
